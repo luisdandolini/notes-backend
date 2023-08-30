b35318dc92047691e7ba6adeee1fcdade98446e8
Luís Fernando Dandolini Duarte <luisfernandodandoliniduarte@gmail.com>
2023-08-30 15:54:46 -0300

    feat: create error handling

--- src/server.js
+++ src/server.js
@@ -1,10 +1,28 @@
 const express = require('express');
 const routes = require('./routes');
+const AppError = require('./utils/AppError');
 
 const app = express();
 app.use(express.json());
 
 app.use(routes);
+
+app.use((error, request, response, next) => {
+  if(error instanceof AppError){
+    return response.status(error.statusCode).json({
+      status: "error",
+      message: error.message
+    });
+  };
+
+  console.error(error);
+
+  return response.status(500).json({
+    status: "error",
+    message: "Internal server error"
+  });
+
+});
 
 const PORT = 3000;
 app.listen(PORT, () => {
