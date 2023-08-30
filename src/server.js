558ab369dc55edd790b8fd44a3d76ad47f39f461
Luís Fernando Dandolini Duarte <luisfernandodandoliniduarte@gmail.com>
2023-08-30 18:17:29 -0300

    feat: connection database

--- src/server.js
+++ src/server.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const routes = require('./routes');
 const AppError = require('./utils/AppError');
+const database = require('./database/sqlite');
 
 const app = express();
 app.use(express.json());
 
 app.use(routes);
+database();
 
 app.use((error, request, response, next) => {
   if(error instanceof AppError){
