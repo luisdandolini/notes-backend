899b32ce9a79d94808448a952447d4da0369dd09
Luís Fernando Dandolini Duarte <luisfernandodandoliniduarte@gmail.com>
2023-08-30 09:44:09 -0300

    feat: create path routes

--- src/server.js
+++ src/server.js
@@ -1,19 +1,12 @@
 const express = require('express');
+const routes = require('./routes');
+
 const app = express();
+app.use(express.json());
+
+app.use(routes);
+
 const PORT = 3000;
-
-app.get('/message/:id/:user', (req, res) => {
-  const { id, user } = req.params;
-
-  res.send(`Mensagem ID: ${id}. Para o usuário ${user}`)
-});
-
-app.get('/users', (req, res) => {
-  const { page, limit } = req.query;
-
-  res.send(`Página: ${page} e Limite: ${limit}`)
-})
-
 app.listen(PORT, () => {
   console.log(`Server is running on PORT: ${PORT}`);
 });
