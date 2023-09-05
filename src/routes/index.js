c594594b1f4ea4319361d61567e9d1fd8ab08f2b
Luís Fernando Dandolini Duarte <luisfernandodandoliniduarte@gmail.com>
2023-09-05 15:37:18 -0300

    chore: method index add search for notes tags

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,6 +2,7 @@ const { Router } = require('express');
 
 const usersRouter = require('./users.routes');
 const notesRouter = require('./notes.routes');
+const tagsRouter = require('./tags.routes');
 
 const routes = Router();
 
@@ -10,5 +11,8 @@ routes.use('/users', usersRouter);
 
 //Notes
 routes.use('/notes', notesRouter);
+
+//Tags
+routes.use('/tags', tagsRouter);
 
 module.exports = routes;
