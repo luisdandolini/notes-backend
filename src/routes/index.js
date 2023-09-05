2c4e880bb859ed6f760032581741a1204aafee30
Luís Fernando Dandolini Duarte <luisfernandodandoliniduarte@gmail.com>
2023-09-05 14:13:08 -0300

    feat: created method for create notes

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,14 @@
 const { Router } = require('express');
 
 const usersRouter = require('./users.routes');
+const notesRouter = require('./notes.routes');
+
 const routes = Router();
 
+// Users
 routes.use('/users', usersRouter);
+
+//Notes
+routes.use('/notes', notesRouter);
 
 module.exports = routes;
